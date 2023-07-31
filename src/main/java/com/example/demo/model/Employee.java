c5466d3cdb98b90a945e96b3e7c343981fe57e1f
ManojShivva <shivvamanojkumar8888@gmail.com>
2023-07-31 23:20:17 +0530

    First commit

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,56 @@
+package com.example.demo.model;
+
+import java.io.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "employees")
+
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "email_id")
+	private String emailID;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String emailID) {
+		super();
+		this.firstName = firstName;
+		this.emailID = emailID;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	
+	
+}
